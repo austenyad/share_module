0627274b1368bd404eb3f5a8617d932a7f33db2c
austenYang <austenyad@gmail.com>
2023-10-31 01:20:00 +0800

    添加子模块完成并测试

---
+++ src/main/java/com/auste/share/Util.kt
@@ -0,0 +1,17 @@
+package com.auste.share
+
+/**
+ * Company: TAO_LE
+ * ================================================
+ * Description:
+ * <p>
+ * <p>
+ * Created by austenYang on 2023/10/31
+ * ================================================
+ */
+object Util {
+
+    fun add(a: Int, b: Int): Int {
+        return a + b;
+    }
+}
